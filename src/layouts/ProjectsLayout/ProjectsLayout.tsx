d333c1577353689f26192d80de83bf1cd7bbc60b
Michaël Bourjac <michael.bourjac@gmail.com>
2024-10-04 08:06:55 +0200

    refactor: rename projects nav to sidebar

--- src/layouts/ProjectsLayout/ProjectsLayout.tsx
+++ src/layouts/ProjectsLayout/ProjectsLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from '@tanstack/react-router';
-import { ProjectsNav } from './ProjectsNav';
+import { ProjectsSidebar } from './ProjectsSidebar';
 
 export const ProjectsLayout = () => {
   return (
     <main className="flex grow overflow-hidden px-8 pt-0">
       <div className="-mx-8 flex grow gap-8 overflow-auto px-8 pt-4">
-        <ProjectsNav />
+        <ProjectsSidebar />
         <div className="w-full pb-8 pt-[4.5rem]">
           <Outlet />
         </div>
--- src/layouts/ProjectsLayout/ProjectsSidebar.tsx
+++ src/layouts/ProjectsLayout/ProjectsSidebar.tsx
@@ -2,7 +2,7 @@ import { Separator } from '../../components/ui/Separator';
 import { NewProjectModal } from './NewProjectModal';
 import { ProjectsNavLink } from './ProjectsNavLink';
 
-export const ProjectsNav = () => {
+export const ProjectsSidebar = () => {
   return (
     <section className="sticky top-[4.5rem] flex h-[calc(100vh-7.5rem)] w-64 flex-col rounded-3xl border bg-white p-3 text-sm">
       <NewProjectModal />
